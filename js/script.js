15741ae77618e317d30588b80f197972bb0cd683
D4riu5 <R3iji94@gmail.com>
2023-01-16 13:25:42 +0100

    Milestone 0 completed

--- js/script.js
+++ js/script.js
@@ -7,8 +7,40 @@
 
 
 // arrays
+const team = [
+  {
+    fullName: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    profileImg: "wayne-barnett-founder-ceo.jpg"
+  },
+  {
+    fullName: 'Angela Caroll',
+    role: 'Chief Editor',
+    profileImg: "angela-caroll-chief-editor.jpg"
+  },
+  {
+    fullName: 'Walter Gordon',
+    role: 'Office Manager',
+    profileImg: "walter-gordon-office-manager.jpg"
+  },
+  {
+    fullName: 'Angela Lopez',
+    role: 'Social Media Manager',
+    profileImg: "angela-lopez-social-media-manager.jpg"
+  },
+  {
+    fullName: 'Scott Estrada',
+    role: 'Developer',
+    profileImg: "scott-estrada-developer.jpg"
+  },
+  {
+    fullName: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    profileImg: "barbara-ramos-graphic-designer.jpg"
+  },
+];
 
-
+console.log(team);
 
 // functions (independent)
 
