4f324abb5ac763aaad710454b5863e11d5f525ec
D4riu5 <R3iji94@gmail.com>
2023-01-16 13:39:37 +0100

    added BONUS folder with files

---
+++ BONUS/js/script.js
@@ -0,0 +1,60 @@
+// selectors
+const teamList = document.getElementById("TeamList");
+
+
+// counters 
+
+
+
+// arrays
+const team = [
+  {
+    fullName: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    profileImg: "wayne-barnett-founder-ceo.jpg"
+  },
+  {
+    fullName: 'Angela Caroll',
+    role: 'Chief Editor',
+    profileImg: "angela-caroll-chief-editor.jpg"
+  },
+  {
+    fullName: 'Walter Gordon',
+    role: 'Office Manager',
+    profileImg: "walter-gordon-office-manager.jpg"
+  },
+  {
+    fullName: 'Angela Lopez',
+    role: 'Social Media Manager',
+    profileImg: "angela-lopez-social-media-manager.jpg"
+  },
+  {
+    fullName: 'Scott Estrada',
+    role: 'Developer',
+    profileImg: "scott-estrada-developer.jpg"
+  },
+  {
+    fullName: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    profileImg: "barbara-ramos-graphic-designer.jpg"
+  },
+];
+
+console.log(team);
+
+// functions (independent)
+
+
+// functions (dependent)
+
+
+
+// events
+for (let member of team) {
+  console.log("Name: "+ member.fullName + ' Role: ' + member.role + ' picture: ' + member.profileImg);
+  
+  let li = document.createElement("li");
+  li.innerHTML = "Name: " + member.fullName + ' Role: ' + member.role + ' picture: ' + member.profileImg;
+  teamList.appendChild(li);
+
+}
